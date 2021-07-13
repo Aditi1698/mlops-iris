7c30291af24df01994f405823ccf5898d7667676
Aditi <aditishilke@gmail.com>
2021-07-13 10:25:33 +0530

    [added] a new classifier

--- ml_utils.py
+++ ml_utils.py
@@ -1,15 +1,14 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 
 # define a Gaussain NB classifier
-#clf = GaussianNB()
-clf = KNeighborsClassifier(n_neighbors=5)     
-# KNN classifier gave an accuracy of 93.3% and Logistic Regression giving accuracy of 96.7%
-#clf = LogisticRegression(penalty='l2',C=1.0, max_iter=10000)
+clf = GaussianNB()
+
+# define a KNN classifier
+clfr1 = KNeighborsClassifier(n_neighbors=5)
 
 # define the class encodings and reverse encodings
 classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
@@ -23,10 +22,19 @@ def load_model():
     # do the test-train split and train the model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     clf.fit(X_train, y_train)
+    clfr1.fit(X_train,y_train)
 
     # calculate the print the accuracy score
     acc = accuracy_score(y_test, clf.predict(X_test))
-    print(f"Model trained with accuracy: {round(acc, 3)}")
+    print(f"Model trained using GaussianNB classfier with accuracy: {round(acc, 3)}")
+    
+
+    # calculate the print the accuracy score
+    acc_1 = accuracy_score(y_test,clfr1.predict(X_test))
+    print(f"Model trained using KNN classifier with accuracy: {round(acc_1, 3)}")
+
+
+
 
 
 # function to predict the flower using the model
@@ -35,6 +43,10 @@ def predict(query_data):
     prediction = clf.predict([x])[0]
     print(f"Model prediction: {classes[prediction]}")
     return classes[prediction]
+    prediction_1=clf_1.predict([x][0])
+    print(f"Model prediction: {classes[prediction_1]}")
+    return classes[prediction_1]
+
 
 # function to retrain the model as part of the feedback loop
 def retrain(data):
@@ -44,3 +56,4 @@ def retrain(data):
 
     # fit the classifier again based on the new data obtained
     clf.fit(X, y)
+    clfr1.fit(X,y)
